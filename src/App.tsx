3365cb05c81647d5adc25c18ef5a069e9ce91cfe
Rashid-Yusof <rashidyusof196@gmail.com>
2025-05-25 17:32:28 +0800

    testing view page with dummy data

--- src/App.tsx
+++ src/App.tsx
@@ -1,17 +1,80 @@
 import React from 'react';
-// import CreateTable from './components/createTable';
-import CreateTable from './components/createTable';
+import { Box, Typography } from '@mui/material';
+import ViewPage from './pages/view';
+import {Book} from './models/book';
+import {Genre} from './models/genre';
 
 function App() {
-  const test = [
-    { id: 1, name: 'siti', age: 28, email: 'alice@example.com' },
-    { id: 2, name: 'Bob', age: 34, email: 'bob@example.com' },
-    { id: 3, name: 'Charlie', age: 25, email: 'charlie@example.com' }
-  ];
+  const exampleBooks: Book[] = [
+  {
+    id: "1",
+    title: "The Great Gatsby",
+    author: "F. Scott Fitzgerald",
+    isbn: "9780743273565",
+    publicationYear: 1925,
+    genre: Genre.Fiction,
+    copies: 5,
+    isAvailable: true,
+  },
+  {
+    id: "2",
+    title: "1984",
+    author: "George Orwell",
+    isbn: "9780451524935",
+    publicationYear: 1949,
+    genre: Genre.NonFiction,
+    copies: 3,
+    isAvailable: false,
+  },
+  {
+    id: "3",
+    title: "To Kill a Mockingbird",
+    author: "Harper Lee",
+    isbn: "9780060935467",
+    publicationYear: 1960,
+    genre: Genre.Science,
+    copies: 4,
+    isAvailable: true,
+  },
+  {
+    id: "4",
+    title: "The Hobbit",
+    author: "J.R.R. Tolkien",
+    isbn: "9780547928227",
+    publicationYear: 1937,
+    genre: Genre.History,
+    copies: 2,
+    isAvailable: true,
+  },
+  {
+    id: "5",
+    title: "Pride and Prejudice",
+    author: "Jane Austen",
+    isbn: "9780141439518",
+    publicationYear: 1813,
+    genre: Genre.Fiction,
+    copies: 6,
+    isAvailable: false,
+  },
+];
+
+const bookButtons: string[] = ['Edit','Remove']
 
   return (
     <div>
-      {/* <CreateTable data={test} /> */}
+      <Box 
+        display="flex"
+        flexDirection="column"
+        alignItems="center"
+        justifyContent="center"
+        height="100%" // Full height of container
+        mt={2}
+      >
+        <Typography variant="h4" gutterBottom>
+          Library Management App
+        </Typography>
+      </Box>
+      <ViewPage title="Books" tableData={exampleBooks} buttons={bookButtons}/>
     </div>
   );
 }
