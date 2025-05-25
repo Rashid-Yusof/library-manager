553b14b2426f419a4ea8c43f310fc111c2a7c0b3
Rashid-Yusof <rashidyusof196@gmail.com>
2025-05-25 14:37:24 +0800

    instal mui for react and setup project for tsx

---
+++ src/App.tsx
@@ -0,0 +1,19 @@
+import React from 'react';
+// import CreateTable from './components/createTable';
+import CreateTable from './components/createTable';
+
+function App() {
+  const test = [
+    { id: 1, name: 'siti', age: 28, email: 'alice@example.com' },
+    { id: 2, name: 'Bob', age: 34, email: 'bob@example.com' },
+    { id: 3, name: 'Charlie', age: 25, email: 'charlie@example.com' }
+  ];
+
+  return (
+    <div>
+      {/* <CreateTable data={test} /> */}
+    </div>
+  );
+}
+
+export default App;
