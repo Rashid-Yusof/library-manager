83ace63ec4d1dd26e2ef621bdbc5fc68d7c50d60
Rashid-Yusof <rashidyusof196@gmail.com>
2025-05-26 11:55:57 +0800

    firebase instructions

--- src/firebase.ts
+++ src/firebase.ts
@@ -1,6 +1,7 @@
 import { initializeApp } from 'firebase/app';
 import { getFirestore } from 'firebase/firestore';
 
+//replace this with config
 const firebaseConfig = {
   apiKey: "",
   authDomain: "",
