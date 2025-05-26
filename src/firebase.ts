92a070b6cd2e0da19ca0ae2263e0493b1de2a338
Rashid-Yusof <rashidyusof196@gmail.com>
2025-05-26 11:54:28 +0800

    firebase

--- src/firebase.ts
+++ src/firebase.ts
@@ -2,12 +2,12 @@ import { initializeApp } from 'firebase/app';
 import { getFirestore } from 'firebase/firestore';
 
 const firebaseConfig = {
-  apiKey: "AIzaSyABlYBQN_WzzD4LXkFYCIOtqmy9TBl-u2E",
-  authDomain: "library-management-app-93a11.firebaseapp.com",
-  projectId: "library-management-app-93a11",
-  storageBucket: "library-management-app-93a11.firebasestorage.app",
-  messagingSenderId: "87539197575",
-  appId: "1:87539197575:web:9908ecba2ff6f167d77a22"
+  apiKey: "",
+  authDomain: "",
+  projectId: "",
+  storageBucket: "",
+  messagingSenderId: "",
+  appId: ""
 };
 
 const app = initializeApp(firebaseConfig);
